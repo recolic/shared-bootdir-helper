1567e3b5e6dbfe6219d2e04edaf5c51f0173cc0b
Recolic Keghart <root@recolic.net>
2021-10-12 01:15:34 +0800

    update

--- src/shared-bootdir-helper-multi-kparam.sh
+++ src/shared-bootdir-helper-multi-kparam.sh
@@ -10,6 +10,8 @@
 #   to correct kernel parameters. 
 #
 # Usage: ./this.sh /boot/grub/grub.cfg
+#
+# Note that grub.cfg must be auto-generated, to make its format correct. 
 
 source "/etc/shared-bootdir-helper-multi-kparam.cfg" || exit 1
 
@@ -20,23 +22,20 @@ inputfile="$1"
 
 while IFS= read -r line; do
     matched=0
-    if [[ "$line" == *"$placeholder"* ]]; then
+    if [[ "$line" == *"\tlinux\t/"* ]]; then
         for hostname in "${!map_hostname_to_kparam[@]}"; do
             # Assuming that, the kimg filename contains "vmlinuz-xxx-$hostname ", in lowercase. That's important! 
             [[ "$line" == *"-$hostname "* ]] &&
-                echo "$line" | sed "s|$placeholder|${map_kimage_to_kparam[$hostname]}|g" >> "$tmpfile" &&
+                echo "$line" | sed "s|-$hostname .*$|-$hostname ${map_kimage_to_kparam[$hostname]}|g" >> "$tmpfile" &&
                 matched=1 && 
                 break
         done
+        [[ $matched == 0 ]] && echo "Warning: kernel line '$line' doesnt match ANY entry in /etc/shared-bootdir-helper-multi-kparam.cfg" && exit 2
     fi
     [[ $matched == 0 ]] && echo "$line" >> "$tmpfile"
 done < "$inputfile" || exit $?
 
 mv "$tmpfile" "$inputfile" || exit $?
-
-grep "$placeholder" "$inputfile" &&
-    echo "Warning: placeholder '$placeholder' still exists in processed grub.cfg. Have you correctly set the 'map_kimage_to_kparam' of $0? Please double-check! " &&
-    exit 2
 
 exit 0
 
